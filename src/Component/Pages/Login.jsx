245dc0e22e673dc9fcea1f550903bd8d9a8cf6bf
ShahidLandge <104378419+ShahidLandge@users.noreply.github.com>
2022-08-22 09:18:38 +0530

    Update Login.jsx

--- src/Component/Pages/Login.jsx
+++ src/Component/Pages/Login.jsx
@@ -45,11 +45,7 @@ export const Login = () => {
 
   return (
     <>
-<<<<<<< HEAD
       <h2  className="formHeader">Login or Sign up</h2>
-=======
-      <h2>Login or Sign up</h2>
->>>>>>> 2a63db37310a5112ac97ddb778f13c5a7a58cc81
       <form className="form" onSubmit={handleSubmit}>
         <br />
         <br />
@@ -99,4 +95,5 @@ export const Login = () => {
     </>
   );
 };
+
 
